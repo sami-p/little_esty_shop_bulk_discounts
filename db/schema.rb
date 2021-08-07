7377ade2803dd4034c1175b84eb7229ac28c394e
sami-p <sami.peterson14@gmail.com>
2021-08-06 21:28:08 -0500

    Create discounts migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_204649) do
+ActiveRecord::Schema.define(version: 2021_08_07_022714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2021_03_04_204649) do
     t.string "city"
     t.string "state"
     t.bigint "zip"
+  end
+
+  create_table "discounts", force: :cascade do |t|
+    t.integer "percent"
+    t.integer "quantity_threshold"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "invoice_items", force: :cascade do |t|
