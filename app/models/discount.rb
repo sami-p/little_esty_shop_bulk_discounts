c218629c5c9a6830ed3c04158677639d2912018f
sami-p <sami.peterson14@gmail.com>
2021-08-07 18:36:06 -0500

    Create Discount model validations for percent and quantity threshold

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,3 +1,7 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
+
+  validates :percent, presence: true, numericality: { greater_than: 0 } 
+  validates :quantity_threshold, numericality: true, presence: true
+
 end
