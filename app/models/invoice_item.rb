4c7116d8ccf265fd3b7f2b678b69905d59801188
sami-p <sami.peterson14@gmail.com>
2021-08-10 21:20:33 -0500

    Create discount helper methods

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -14,4 +14,30 @@ class InvoiceItem < ApplicationRecord
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
   end
+
+  # invoice items
+    # unit price
+    # quantity
+    # item_id
+
+  # merchant
+    # id
+
+  # item
+    # merchant_id
+
+  # discount
+    # percent
+    # quantity threshold
+    # merchant_id
+
+  def apply_discount?
+    merchant = Merchant.where("id = ?", item.merchant_id)
+    self.quantity >= merchant.first.discounts.minimum(:quantity_threshold)
+  end
+
+  def select_discount
+    merchant = Merchant.where("id = ?", item.merchant_id).first
+    wip = merchant.discounts.where("quantity_threshold <= ?", self.quantity).order('percent DESC').first
+  end
 end
